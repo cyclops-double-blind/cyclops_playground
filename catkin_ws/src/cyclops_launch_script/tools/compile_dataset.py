f867aa0e362a7844157f42cdce365fd57e5fdfcc
cyclops-double-blind <cyclops.double.blind@gmail.com>
2025-07-01 19:30:15 +0900

    refactor(script): fix case conventions

--- catkin_ws/src/cyclops_launch_script/tools/compile_dataset.py
+++ catkin_ws/src/cyclops_launch_script/tools/compile_dataset.py
@@ -18,20 +18,20 @@ from geometry_msgs.msg import Vector3, Point, Quaternion, PoseStamped
 cv_bridge = CvBridge()
 
 
-def parse_data_csv(root):
-    with open(join(root, "data.csv"), "r") as data_csv_f:
-        lines = data_csv_f.readlines()
+def parseDataCSV(root):
+    with open(join(root, "data.csv"), "r") as f:
+        lines = f.readlines()
     return [[e for e in line.strip().split(",")] for line in lines[1:]]
 
 
-def parse_timestamp(timestamp):
+def parseTimestamp(timestamp):
     onesec_in_ns = 1000000000
     sec = timestamp // onesec_in_ns
     nsec = timestamp % onesec_in_ns
     return rospy.Time(sec, nsec)
 
 
-def parse_image_target_encoding(cv_image):
+def parseImageTargetEncoding(cv_image):
     def handle_mono_image():
         if cv_image.dtype in (np.int8, np.uint8):
             return "mono8"
@@ -52,27 +52,27 @@ def parse_image_target_encoding(cv_image):
     raise ValueError("Intractable image channel width: {}".format(channel))
 
 
-def parse_image(timestamp, filepath):
+def parseImage(timestamp, filepath):
     cv_image = cv2.imread(filepath, cv2.IMREAD_UNCHANGED)
-    encoding = parse_image_target_encoding(cv_image)
+    encoding = parseImageTargetEncoding(cv_image)
 
     image = cv_bridge.cv2_to_imgmsg(cv_image, encoding=encoding)
-    image.header.stamp = parse_timestamp(timestamp)
+    image.header.stamp = parseTimestamp(timestamp)
 
     return image
 
 
-def parse_imu(timestamp, rotation, acceleration):
+def parseIMU(timestamp, rotation, acceleration):
     msg = Imu()
-    msg.header.stamp = parse_timestamp(timestamp)
+    msg.header.stamp = parseTimestamp(timestamp)
     msg.angular_velocity = Vector3(*rotation)
     msg.linear_acceleration = Vector3(*acceleration)
     return msg
 
 
-def parse_posestamped(frame, timestamp, position, orientation):
+def parsePosestamped(frame, timestamp, position, orientation):
     msg = PoseStamped()
-    msg.header.stamp = parse_timestamp(timestamp)
+    msg.header.stamp = parseTimestamp(timestamp)
     msg.header.frame_id = frame
     msg.pose.position = Point(*position)
 
@@ -81,24 +81,24 @@ def parse_posestamped(frame, timestamp, position, orientation):
     return msg
 
 
-def parse_vector(data, col_start, col_end):
+def parseVector(data, col_start, col_end):
     return [tuple(float(_) for _ in line[col_start:col_end]) for line in data]
 
 
-def read_start_time(root):
-    data = parse_data_csv(root)
+def readStartTime(root):
+    data = parseDataCSV(root)
     return int(data[0][0])
 
 
-def read_camera(root, time_offset):
-    print("reading camera data: {}".format(root))
+def readCamera(root, time_offset):
+    print("Reading camera data: {}".format(root))
 
-    data = parse_data_csv(root)
+    data = parseDataCSV(root)
 
     def make_result(progress=None):
         result = [None] * len(data)  # type: list
-        for (i, (timestamp, filename)) in enumerate(data):
-            result[i] = parse_image(
+        for i, (timestamp, filename) in enumerate(data):
+            result[i] = parseImage(
                 int(timestamp) - time_offset, join(root, "data", filename)
             )
             if progress is not None:
@@ -112,43 +112,43 @@ def read_camera(root, time_offset):
         return make_result(progress)
 
 
-def read_imu(root, time_offset):
-    print("reading IMU data: {}".format(root))
+def readIMU(root, time_offset):
+    print("Reading IMU data: {}".format(root))
 
-    data = parse_data_csv(root)
+    data = parseDataCSV(root)
     return [
-        parse_imu(*entry)
+        parseIMU(*entry)
         for entry in zip(
             [int(line[0]) - time_offset for line in data],
-            parse_vector(data, 1, 4),
-            parse_vector(data, 4, 7),
+            parseVector(data, 1, 4),
+            parseVector(data, 4, 7),
         )
     ]
 
 
-def read_groundtruth(root, time_offset, frame):
-    print("reading ground truth data: {}".format(root))
+def readGroundtruth(root, time_offset, frame):
+    print("Reading ground truth data: {}".format(root))
 
-    data = parse_data_csv(root)
+    data = parseDataCSV(root)
     return [
-        parse_posestamped(frame, *entry)
+        parsePosestamped(frame, *entry)
         for entry in zip(
             [int(line[0]) - time_offset for line in data],
-            parse_vector(data, 1, 4),
-            parse_vector(data, 4, 8),
+            parseVector(data, 1, 4),
+            parseVector(data, 4, 8),
         )
     ]
 
 
-def process_dataset(data_root, timestamp_base, groundtruth_stream):
-    start_time = read_start_time(join(data_root, "imu0"))
+def processDataset(data_root, timestamp_base, groundtruth_stream):
+    start_time = readStartTime(join(data_root, "imu0"))
     time_offset = start_time - timestamp_base
 
-    imu_data = read_imu(join(data_root, "imu0"), time_offset)
-    truth_estim_data = read_groundtruth(
+    imu_data = readIMU(join(data_root, "imu0"), time_offset)
+    truth_estim_data = readGroundtruth(
         join(data_root, groundtruth_stream), time_offset, "map"
     )
-    camera_data = read_camera(join(data_root, "cam0"), time_offset)
+    camera_data = readCamera(join(data_root, "cam0"), time_offset)
 
     return {
         "camera": camera_data,
@@ -157,18 +157,18 @@ def process_dataset(data_root, timestamp_base, groundtruth_stream):
     }
 
 
-def write_output(data, output):
+def writeOutput(data, output):
     if tqdm is None:
-        for (topic, msgs) in data.items():
+        for topic, msgs in data.items():
             for msg in msgs:
                 output.write(topic, msg, msg.header.stamp)
     else:
-        for (topic, msgs) in tqdm(data.items(), unit="topic"):
+        for topic, msgs in tqdm(data.items(), unit="topic"):
             for msg in tqdm(msgs, unit="message"):
                 output.write(topic, msg, msg.header.stamp)
 
 
-def parse_args():
+def parseArgs():
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument(
         "--output_bag",
@@ -200,14 +200,14 @@ def parse_args():
 
 
 def main():
-    args = parse_args()
-    data = process_dataset(
+    args = parseArgs()
+    data = processDataset(
         args.data_dir, args.timestamp_base, args.groundtruth_stream_name
     )
 
-    print("writing output")
+    print("Writing output")
     with Bag(args.output_bag, "w") as output:
-        write_output(data, output)
+        writeOutput(data, output)
 
 
 if __name__ == "__main__":
